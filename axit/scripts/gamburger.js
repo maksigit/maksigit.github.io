890bccb1c6644f86963b93952e093f90dd2ec4ff
Maksim Kholod <maksi123p@gmail.com>
2017-11-10 16:09:19 +0200

    ready axit

--- axit/scripts/gamburger.js
+++ axit/scripts/gamburger.js
@@ -3,5 +3,20 @@ $('.gamburger').on('click', function () {
 });
 
 $('.btn.try').on('click', function () {
-    $('.wrap-form-header').slideToggle();
-})
+    $('.wrap-form-header').addClass('active-form');
+});
+
+$('.close').on('click', function () {
+
+    $('.wrap-form-header').removeClass('active-form');
+});
+
+// $(document).on('click', '.wrap-form-header', function (e) {
+//     var clickedEl = e.target.className;
+//
+//     if(clickedEl == 'wrap-form-header active-form') {
+//         $(e.target).removeClass('active-form');
+//     }
+// });
+
+
