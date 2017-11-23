c781ff25055ea1dd434477bf5efaecb9d264028f
Maksim Kholod <maksi123p@gmail.com>
2017-11-23 14:51:34 +0200

    corrected gamburger

--- brick/scripts/gamburger.js
+++ brick/scripts/gamburger.js
@@ -1,5 +1,11 @@
 // drop down the menu, and swap the icon to the close icon
 $('.gamberger').click(function(){
     $('.main-nav').slideToggle();
+
+});
+
+$('.hide').click(function(){
+    $('.main-nav').slideToggle();
+
 });
 
