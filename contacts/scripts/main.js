0cd96d611a48d1537598ad28faad51d65e474156
Maksim Kholod <maksi123p@gmail.com>
2017-12-10 16:43:36 +0200

    edit contacts

--- contacts/scripts/main.js
+++ contacts/scripts/main.js
@@ -5,24 +5,24 @@ $('.mobile-menu').on('click', function () {
 
 // owl-slider
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    dots:false,
-    navText : ["<",">"],
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
+    loop: true,
+    margin: 10,
+    dots: false,
+    navText: ["<", ">"],
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
         },
-        600:{
-            items:1,
-            nav:true
+        600: {
+            items: 1,
+            nav: true
         },
-        1000:{
-            items:1,
-            nav:true,
-            loop:false
+        1000: {
+            items: 1,
+            nav: true,
+            loop: false
         }
     }
 })
