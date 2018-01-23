b91a9dca4bfdf223c0515b7604d7f3f44d88ca50
maksigit <maksi123p@gmail.com>
2018-01-23 16:18:39 +0200

    del fol

--- milli/scripts/menu-hide.js
+++
@@ -1,9 +0,0 @@
-$('.menu-hide').on('click', function () {
-    $('.nav-menu').toggleClass('to-show');
-    $('.fa-bars').toggleClass('fa-times');
-    $('.fa-times').toggleClass('fa-bars');
-});
-
-$('.close-nav').on('click', function () {
-    $('.nav-menu').toggleClass('to-show');
-});
