b997673865b0a1157a6764e787803f1ec4f74949
maksigit <maksi123p@gmail.com>
2018-02-13 20:07:48 +0200

    add remont

---
+++ remont/scripts/menu-hide.js
@@ -0,0 +1,8 @@
+$('.mobile-menu').on('click', function () {
+    $('.menu-setting').show();
+    $('.close-mobile-menu').show();
+});
+
+$('.close-mobile-menu').on('click', function () {
+    $('.menu-setting').hide();
+})
---
+++ remont/scripts/owl.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: true,
+    nav: false,
+    responsiveClass: true,
+    // navText: ["<", ">"],
+    responsive: {
+        0: {
+            items: 1,
+            // nav: false,
+            dots: true
+        },
+        600: {
+            items: 1,
+            // nav: true
+        },
+        1000: {
+            items: 1,
+            // nav: true
+        }
+    }
+});
