5b05e0b6d43137d876fd6561fe70abd97c3bdbf5
maksigit <maksi123p@gmail.com>
2018-02-13 20:17:52 +0200

    add remont fix

--- remont/scripts/owl.js
+++ remont/scripts/owl.js
@@ -4,20 +4,16 @@ $('.owl-carousel').owlCarousel({
     dots: true,
     nav: false,
     responsiveClass: true,
-    // navText: ["<", ">"],
     responsive: {
         0: {
             items: 1,
-            // nav: false,
             dots: true
         },
         600: {
-            items: 1,
-            // nav: true
+            items: 1
         },
         1000: {
-            items: 1,
-            // nav: true
+            items: 1
         }
     }
 });
