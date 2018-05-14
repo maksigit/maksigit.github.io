6b29dd2bbbd6509d194b8ea463f52c5d6a840de9
maksigit <maksi123p@gmail.com>
2018-05-14 13:48:36 +0300

    refactoring todoList

--- education/todo.js
+++ education/todo.js
@@ -5,13 +5,14 @@ function setToLoc(o) {
 }
 
 let idM = 0;
+
 function getId(arr) {
     let maxArr = [];
-    if(!arr.length) {
+    if (!arr.length) {
         maxArr.push(0)
     }
     else {
-        for(let i = 0; i < arr.length; i++) {
+        for (let i = 0; i < arr.length; i++) {
             maxArr.push(arr[i].id)
         }
     }
@@ -21,156 +22,128 @@ function getId(arr) {
 
 let button = document.querySelector('.send-task');
 let listOutput = document.querySelector('.list-output');
-let testTrue = true;
 let item = JSON.parse(localStorage.getItem('item')) || [];
 let itemLenght = item.length;
-console.log(item);
 
+function eventRemoveOnButton (e) {
+    e.preventDefault();
+    let allThrough = document.querySelectorAll('.line-through');
+    let allThoughtLenght = allThrough.length;
+    let attr = [];
+    for (let i = 0; i < allThoughtLenght; i++) {
+        attr.push(allThrough[i].attributes[0].nodeValue);
+    }
+
+    let arrSort = item.filter(function (num) {
+        for (let i = 0; i < attr.length; i++) {
+            if (num.id == attr[i]) {
+                return false
+            }
+        }
+        return true;
+    });
+
+    item = arrSort;
+    for (let i = 0; i < allThoughtLenght; i++) {
+        allThrough[i].remove()
+    }
+    setToLoc(item);
+}
 
 function buttonOn() {
     let removeButton = document.createElement('button');
     removeButton.className = "remove-button";
     removeButton.innerHTML = 'Удалить выбранные';
-    removeButton.addEventListener('click', function (e) {
-        e.preventDefault();
-        let allThrough = document.querySelectorAll('.line-through');
-        console.log(allThrough);
-        let allThoughtLenght = allThrough.length;
-        let attr = [];
-        for (let i = 0; i < allThoughtLenght; i++) {
-            attr.push(allThrough[i].attributes[0].nodeValue);
-            console.log(attr);
-        }
-
-        let arrSort = item.filter(function (num) {
-            for(let i = 0; i < attr.length; i++) {
-                if (num.id == attr[i]) {
-                    return false
-                }
-            }
-            return true;
-        });
-
-        item = arrSort;
-        for(let i = 0; i < allThoughtLenght; i++) {
-            allThrough[i].remove()
-        }
-        setToLoc(item);
-    });
+    removeButton.addEventListener('click', eventRemoveOnButton);
     listOutput.appendChild(removeButton);
 }
-
-
-
 
 button.addEventListener('click', function (e) {
     e.preventDefault();
     let getInput = document.getElementsByClassName('input-task')[0];
     let getInputValue;
-    if(getInput.value) {
+    if (getInput.value) {
         getInputValue = getInput.value;
-        let itemObj = {text: getInputValue, id: getId(item), flag: false};
-
+        let itemObj = {id: getId(item), title: getInputValue,  completed: false};
         item.push(itemObj);
         itemLenght = item.length;
-
         getInput.value = "";
         setToLoc(item);
-
         createLi(itemObj);
     } else {
         alert("INPUT TASK")
     }
-
 });
-function creatLIFromArray(value) { // value === [] or value === {}
 
-    for(let i = 0; i < value.length; i++ ) {
+function creatLIFromArray(value) { // value === [] or value === {}
+    for (let i = 0; i < value.length; i++) {
         createLi(value[i])
+    }
+}
+function handleLiOnClick(event)  {
+    let getId = this.getAttribute('data');
+    for (let i = 0; i < item.length; i++) {
+        if (item[i].id == getId) {
+
+            if (item[i].completed) {
+                item[i].completed = false
+            } else {
+                item[i].completed = true
+            }
+        }
+    }
+    setToLoc(item);
+    item = JSON.parse(localStorage.getItem('item'));
+    let test = item.find(function (element) {
+        return element.completed
+    });
+    if (test) {
+        buttonOn()
+    } else {
+        document.querySelector('.remove-button').remove()
+    }
+    if (this.className === "line-through") {
+        this.className = "item";
+    } else {
+        this.className = "line-through";
     }
 }
 
 function createLi(val) {
-
     let newLi = document.createElement('li');
-        newLi.setAttribute('data', val.id);
+    newLi.setAttribute('data', val.id);
     let newInput = document.createElement('input');
-
     let close = document.createElement('span');
     close.className = "close";
-
     newInput.setAttribute('type', 'checkbox');
     newInput.className = 'check';
-    newLi.addEventListener('click', function (event) {
-        let getId = this.getAttribute('data');
-        console.log(getId);
-
-        for (let i = 0; i < itemLenght; i++) {
-            if(item[i].id == getId) {
-
-                if(item[i].flag) {
-                    item[i].flag = false
-                } else {
-                    item[i].flag = true
-                }
-            }
-        }
-        setToLoc(item);
-
-        item = JSON.parse(localStorage.getItem('item'));
-
-        let test = item.find(function (element) {
-                return element.flag
-        });
-
-        console.log(test);
-
-        if (test) {
-            buttonOn()
-        } else {
-            document.querySelector('.remove-button').remove()
-        }
-
-        console.log(item);
-        if (this.className === "line-through") {
-            this.className = "item";
-        } else {
-            this.className = "line-through";
-        }
-    });
-
+    newLi.addEventListener('click', handleLiOnClick);
     newLi.className = "item";
-    newLi.innerHTML = val.text;
-
+    newLi.innerHTML = val.title;
     newLi.appendChild(newInput);
     newLi.appendChild(close);
-
     listOutput.appendChild(newLi);
 }
 
 listOutput.addEventListener('click', function (e) {
-    if(e.target.className === 'close') {
+    if (e.target.className === 'close') {
         let takeID = e.target.parentElement.getAttribute('data');
-            console.log(takeID);
-            let newArr = [];
-            for(let i = 0; i < itemLenght; i++) {
-                console.log(item[i].id);
-                if (item[i].id == takeID) {
-                    console.log('ggg');
-                } else {
-                    newArr.push(item[i]);
-                }
+        let newArr = [];
+        for (let i = 0; i < itemLenght; i++) {
+            if (item[i].id == takeID) {
+                console.log('ggg');
+            } else {
+                newArr.push(item[i]);
             }
-            item = newArr;
-            itemLenght = newArr.length;
-            setToLoc(newArr);
+        }
+        item = newArr;
+        itemLenght = newArr.length;
+        setToLoc(newArr);
         e.target.parentElement.remove();
     } else {
         console.log('ggg')
     }
 });
-
-
 
 creatLIFromArray(item);
 
