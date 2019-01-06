c6d5b35e52f12d31232db6997f2e633d65d6025c
maksigit <maksi123p@gmail.com>
2019-01-06 15:41:22 +0200

    add promise and delete one button

--- imagga/README.md
+++ imagga/README.md
@@ -2,4 +2,7 @@
 2. cd to folder imaggapp
 3. npm i
 4. npm start
-5. enjoy =)
+5. enter img(url) in input tag (example: https://www-europe.infiniti-cdn.net/content/dam/Infiniti/EMEA_ONLY/vehicles/Q30/Q30_C30_NAX_1536x864-1_v2_red.png.ximg.s_12_h.smart.png)
+6. 
+
+
--- imagga/imaggapp/src/App.js
+++ imagga/imaggapp/src/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import './App.css'
 
 let parseBody;
 let tagsArr;
+
 class App extends Component {
 
   state = {
     tags: []
   };
 
-  upDateState (newState) {
+  upDateState(newState) {
     this.setState({
       tags: [newState[0], ...this.state.tags],
     });
@@ -22,28 +23,36 @@ class App extends Component {
       apiSecret = 'b6384a644354b467ffc7e3cffdf86b24',
       imageUrl = urlImagga.value;
 
-    request.get('https://api.imagga.com/v1/tagging?url='+encodeURIComponent(imageUrl), function (error, response, body) {
-      parseBody = JSON.parse(body);
-      tagsArr = parseBody.results;
-    }).auth(apiKey, apiSecret, true);
+    let promise = new Promise((resolve, reject) => {
+
+      request.get('https://api.imagga.com/v1/tagging?url=' + encodeURIComponent(imageUrl), function (error, response, body) {
+        parseBody = JSON.parse(body);
+        tagsArr = parseBody.results;
+        resolve(tagsArr);
+      }).auth(apiKey, apiSecret, true);
+    });
+    promise.then(result => (this.upDateState(result))
+    );
   }
 
   render() {
     return (
       <div className="App">
-        <input  placeholder="Input IMG(URL)" type="text" ref={(input) => {this.urlImagga = input}}/>
-        <button onClick={() => this.getTags(this.urlImagga)}>Upload</button>
-        <button onClick={() => this.upDateState(tagsArr)}>Recognize</button>
+        <input placeholder="Input IMG(URL)" type="text" ref={(input) => {
+          this.urlImagga = input
+        }}/>
+        <button onClick={() => this.getTags(this.urlImagga)}>Recognize</button>
         {this.state.tags.map((item, index) => {
           return <div key={index} className="wrap-response">
             <div className="wrap-img"><img src={item.image} alt=""/></div>
             <ul key={index} className="wrap-item">{item.tags.map((item, index) => {
               return <li key={index} className="item">
-                      <span >{item.tag}</span>
-                      <span>{item.confidence}</span>
-                     </li>
+                <span className="tag">{item.tag}</span>
+                <span className="confidence">{item.confidence} %</span>
+                <span className="back-percent" style={{width: item.confidence + '%'}}> </span>
+              </li>
             })}
-          </ul>
+            </ul>
           </div>
         })}
       </div>
