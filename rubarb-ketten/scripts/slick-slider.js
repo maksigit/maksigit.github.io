559e82e792672233ed82abf9f1800208e4f340ae
maksigit <maksi123@ukr.net>
2020-06-02 23:46:34 +0300

    add rubarb ketten

---
+++ rubarb-ketten/scripts/slick-slider.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('.banner__slider').slick({
+        // infinite: false,
+        // arrows: false,
+        // autoplay: true,
+        prevArrow:'<svg class="slick-prev" width="71" height="40" viewBox="0 0 71 40" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+            '<line x1="71" y1="19.834" x2="1.34937" y2="19.834" stroke="white" stroke-opacity="0.2"/>\n' +
+            '<line x1="71" y1="19.834" x2="1.34937" y2="19.834" stroke="white" stroke-opacity="0.2"/>\n' +
+            '<line x1="20.8827" y1="0.491493" x2="1.35354" y2="20.0207" stroke="white" stroke-opacity="0.2"/>\n' +
+            '<line x1="20.8827" y1="0.491493" x2="1.35354" y2="20.0207" stroke="white" stroke-opacity="0.2"/>\n' +
+            '<line x1="20.1756" y1="39.3536" x2="0.646425" y2="19.8244" stroke="white" stroke-opacity="0.2"/>\n' +
+            '<line x1="20.1756" y1="39.3536" x2="0.646425" y2="19.8244" stroke="white" stroke-opacity="0.2"/>\n' +
+            '</svg>\n',
+        nextArrow:'<svg class="slick-next" width="71" height="40" viewBox="0 0 71 40" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+            '<line y1="20.166" x2="69.6506" y2="20.166" stroke="white" stroke-opacity="0.2"/>\n' +
+            '<line y1="20.166" x2="69.6506" y2="20.166" stroke="white" stroke-opacity="0.2"/>\n' +
+            '<line x1="50.1173" y1="39.5085" x2="69.6464" y2="19.9793" stroke="white" stroke-opacity="0.2"/>\n' +
+            '<line x1="50.1173" y1="39.5085" x2="69.6464" y2="19.9793" stroke="white" stroke-opacity="0.2"/>\n' +
+            '<line x1="50.8244" y1="0.646447" x2="70.3536" y2="20.1756" stroke="white" stroke-opacity="0.2"/>\n' +
+            '<line x1="50.8244" y1="0.646447" x2="70.3536" y2="20.1756" stroke="white" stroke-opacity="0.2"/>\n' +
+            '</svg>\n'
+    });
+});
